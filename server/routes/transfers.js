0876577049ee4bfee22b52d2ee77cab3532addc2
Viktor Ohad <ygatravaz@protonmail.com>
2019-01-28 23:01:03 +0100

    ex-5

--- server/routes/transfers.js
+++ server/routes/transfers.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
 const express = require('express');
 const router = express.Router();
-const collection = require('lodash/collection'); 
+const collection = require('lodash/collection');
+const csurf = require('csurf');
 
 const Db = require('../db').instance;
 const Account = Db.models.account;
@@ -13,29 +14,44 @@ function errorAndReload(req, res, message) {
   req.session.sessionFlash = {
     type: 'danger',
     message
-  }
+  };
   res.redirect('/transfers');
 }
+
+const csurfMW = csurf({
+  cookie: true
+});
+
 //////////////////////////////////////////////////////////////
 //// ↓ EXERCISE 5 SOLUTION GOES HERE
 ////   - Add CSRF protection to this route
-router.get('/', function(req, res/*, next*/) {
+router.get('/', csurfMW, function(req, res /*, next*/) {
   bounceOutIfLoggedOut(req, res, () => {
     let { accountTo, accountFrom, amount } = req.query;
     Account.findAll({
       attributes: ['id', 'number', 'name', 'userId', 'balance'],
-      include: [{
-        model: User,
-        where: { id: Sequelize.col('userId') }
-      }]
+      include: [
+        {
+          model: User,
+          where: { id: Sequelize.col('userId') }
+        }
+      ]
     }).then(allAccounts => {
-      let myAccounts = allAccounts.filter((a) => a.userId === req.session.currentUser.id);
-      let userAccounts = collection.groupBy(
-        allAccounts.map(a => a.get({plain: true})), acc => acc.user.username);
+      let myAccounts = allAccounts.filter(a => a.userId === req.session.currentUser.id);
+      let userAccounts = collection.groupBy(allAccounts.map(a => a.get({ plain: true })), acc => acc.user.username);
       //////////////////////////////////////////////////////////////
       //// ↓ EXERCISE 5 SOLUTION GOES HERE
       ////   - Pass req.csrfToken() to template so it can be rendered
-      res.render('transfers', { title: 'Strawbank: Transfers', myAccounts, userAccounts, accountTo, accountFrom, amount });
+      const csrfToken = req.csrfToken();
+      res.render('transfers', {
+        title: 'Strawbank: Transfers',
+        myAccounts,
+        userAccounts,
+        accountTo,
+        accountFrom,
+        amount,
+        csrfToken
+      });
     });
   });
 });
@@ -44,7 +60,7 @@ router.get('/', function(req, res/*, next*/) {
 //// ↓ EXERCISE 5 SOLUTION GOES HERE
 ////   - Add CSRF protection to this route
 ////   - Limit this route to only POST requests
-router.all('/perform', function(req, res) {
+router.post('/perform', csurfMW, function(req, res) {
   bounceOutIfLoggedOut(req, res, () => {
     let { accountFrom, accountTo, amount } = Object.assign(Object.assign({}, req.body), req.query);
     amount = parseFloat(amount);
@@ -61,32 +77,39 @@ router.all('/perform', function(req, res) {
       return;
     }
 
-    Promise.all(
-      [accountFrom, accountTo]
-        .map(accountId => Account.findById(accountId))
-    ).then(([aFrom, aTo]) => {
-      if (!aFrom || !aTo) {
-        errorAndReload(req, res, 'One or more account IDs are invalid');
-        return;  
-      }
-      if (aFrom.balance < amount) {
-        errorAndReload(req, res, 'Nonsufficient funds in account');
-        return; 
-      }
-      return Db.transaction(t => {
-        return aFrom.update({
-          balance: Math.round(100 * (aFrom.balance - amount)) / 100
-        }, { transaction: t }).then(() => {
-          return aTo.update({
-            balance: Math.round(100 * (aTo.balance + amount)) / 100
-          }, { transaction: t });
+    Promise.all([accountFrom, accountTo].map(accountId => Account.findById(accountId)))
+      .then(([aFrom, aTo]) => {
+        if (!aFrom || !aTo) {
+          errorAndReload(req, res, 'One or more account IDs are invalid');
+          return;
+        }
+        if (aFrom.balance < amount) {
+          errorAndReload(req, res, 'Nonsufficient funds in account');
+          return;
+        }
+        return Db.transaction(t => {
+          return aFrom
+            .update(
+              {
+                balance: Math.round(100 * (aFrom.balance - amount)) / 100
+              },
+              { transaction: t }
+            )
+            .then(() => {
+              return aTo.update(
+                {
+                  balance: Math.round(100 * (aTo.balance + amount)) / 100
+                },
+                { transaction: t }
+              );
+            });
+        }).then(() => {
+          res.redirect('/transfers');
         });
-      }).then(() => {
-        res.redirect('/transfers');
+      })
+      .catch(e => {
+        errorAndReload(req, res, e.message);
       });
-    }).catch((e) => {
-      errorAndReload(req, res, e.message);
-    });
   });
 });
 
